bb48507f01f1bbd4109466f9dabdba77e69652f0
Randy Nguyen <rnguyen@riskalyze.com>
2019-03-03 21:15:04 -0500

    use partials & base template. Also max-temp page

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def comparisons():
     return render_template("comparisons.html")
 
+@app.route('/max-temp')
+def maxTemp():
+    return render_template('max-temp.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
